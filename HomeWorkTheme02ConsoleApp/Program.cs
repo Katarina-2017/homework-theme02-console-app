47317ec9beaf1403dfd6b96faaccc0c59160ffc3
katzhukova <katzhukova@gmail.com>
2021-12-30 18:59:56 +0300

    Добавила комментарии по коду

--- HomeWorkTheme02ConsoleApp/Program.cs
+++ HomeWorkTheme02ConsoleApp/Program.cs
@@ -4,37 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+/// <summary>
+/// Пространство имён
+/// </summary>
 namespace HomeWorkTheme02ConsoleApp
 {
+    /// <summary>
+    /// Главная составляющая единица приложения - класс
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// Код, который выполняется в первую очередь
+        /// </summary>
+        /// <param name="args">Параметры запуска</param>
         static void Main(string[] args)
         {
-            string fullName = "Галимова Екатерина Валерьевна";
+            string fullName = "Галимова Екатерина Валерьевна"; // создание строковых переменных fullName и email
             string email = "katzhukova@gmail.com";
 
-            byte age = 33;
+            byte age = 33; //создание целочисленной переменной age (возраст явно не может превышать 225 лет)
 
-            double programmingScores = 90;
+            double programmingScores = 90; //создание переменных для хранения баллов, как постоянные числа с плавающей точкой
             double mathScores = 50;
             double physicsScores = 30;
 
             Console.WriteLine($"ФИО: {fullName}; \nEmail: {email}; \nВозраст: {age}; " +
-                $"\nБаллы по программированию: {programmingScores}; \nБаллы по математике: {mathScores}; \nБаллы по физике: {physicsScores}.");
+                $"\nБаллы по программированию: {programmingScores}; \nБаллы по математике: {mathScores}; \nБаллы по физике: {physicsScores}."); //форматированный вывод на экран
 
-            Console.ReadKey();
+            Console.ReadKey(); //задержка экрана
 
-            double amountOfScores;
+            double amountOfScores; //создание переменной для хранения суммы баллов, как постоянные числа с плавающей точкой
 
-            amountOfScores = programmingScores + mathScores + physicsScores;
+            amountOfScores = programmingScores + mathScores + physicsScores; //нахождение суммы баллов
 
-            double averageScore;
+            double averageScore; // создание переменной под средний балл, как постоянные числа с плавающей точкой
 
-            averageScore = amountOfScores / 3;
+            averageScore = amountOfScores / 3; //нахождение среднего балла 
 
-            Console.WriteLine($"\nСумма баллов по всем предметам:{amountOfScores}; \nСреднее арифметическое баллов: {averageScore:#.##}.");
+            Console.WriteLine($"\nСумма баллов по всем предметам:{amountOfScores}; \nСреднее арифметическое баллов: {averageScore:#.##}."); //форматированный вывод на экран
 
-            Console.ReadKey();
+            Console.ReadKey(); //задержка экрана
         }
     }
 }
